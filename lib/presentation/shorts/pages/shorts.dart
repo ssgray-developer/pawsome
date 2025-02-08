08073954d4bf44f099da4613cd9a1f0c74b2bf9b
unknown <ssgray-developer@gmail.com>
2025-02-08 18:09:28 +0800

    Added video auto play after scrolling

--- lib/presentation/shorts/pages/shorts.dart
+++ lib/presentation/shorts/pages/shorts.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:pawsome/presentation/shorts/widgets/video_player_item.dart';
+
+import '../widgets/video_player_item.dart';
 
 class ShortsScreen extends StatefulWidget {
   const ShortsScreen({super.key});
 
   static List<String> videoUrls = [
-    "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4", // Replace with actual video URLs
+    "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
     "https://videos.pexels.com/video-files/6853337/6853337-sd_506_960_25fps.mp4",
     "https://videos.pexels.com/video-files/4624594/4624594-sd_360_640_30fps.mp4",
-    // Add more video URLs
   ];
 
   @override
@@ -16,21 +16,32 @@ class ShortsScreen extends StatefulWidget {
 }
 
 class _ShortsScreenState extends State<ShortsScreen> {
-  bool isLoading = false;
+  final PageController pageController = PageController();
+  Map<String, double> videoPositions = {};
+
+  void updateVideoPosition(String videoUrl, double position) {
+    setState(() {
+      videoPositions[videoUrl] = position;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: ShortsScreen.videoUrls.length,
-        physics: isLoading ? const NeverScrollableScrollPhysics() : null,
-        itemBuilder: (context, index) {
-          return VideoPlayerItem(
-            videoUrl: ShortsScreen.videoUrls[index],
-            title: 'hsdbfiugubfiouseboifvodiadiufewiofaorfihaoifuh',
-            description: 'adsiouvnidunvpoaidvbiaudsbis',
-            profileUrl: 'https://pixhost.icu/avaxhome/ae/42/00a642ae.jpg',
-          );
-        });
+      controller: pageController,
+      scrollDirection: Axis.vertical,
+      itemCount: ShortsScreen.videoUrls.length,
+      itemBuilder: (context, index) {
+        String videoUrl = ShortsScreen.videoUrls[index];
+        return VideoPlayerItem(
+          videoUrl: videoUrl,
+          title: 'Video Title $index',
+          description: 'Description of Video $index',
+          profileUrl: 'https://pixhost.icu/avaxhome/ae/42/00a642ae.jpg',
+          savedPosition: videoPositions[videoUrl],
+          onPositionChanged: updateVideoPosition,
+        );
+      },
+    );
   }
 }
--- lib/presentation/shorts/widgets/video_player_item.dart
+++ lib/presentation/shorts/widgets/video_player_item.dart
@@ -6,12 +6,16 @@ import 'package:video_player/video_player.dart';
 import 'package:marquee/marquee.dart';
 import 'package:pawsome/core/theme/app_colors.dart';
 import 'package:pawsome/core/theme/app_values.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPlayerItem extends StatefulWidget {
   final String videoUrl;
   final String title;
   final String description;
   final String profileUrl;
+  final double? savedPosition;
+  final Function(String, double)
+      onPositionChanged; // Callback to update position
 
   const VideoPlayerItem({
     super.key,
@@ -19,30 +23,42 @@ class VideoPlayerItem extends StatefulWidget {
     required this.title,
     required this.description,
     required this.profileUrl,
+    this.savedPosition,
+    required this.onPositionChanged,
   });
 
   @override
   State<VideoPlayerItem> createState() => _VideoPlayerItemState();
 }
 
-class _VideoPlayerItemState extends State<VideoPlayerItem> {
+class _VideoPlayerItemState extends State<VideoPlayerItem>
+    with AutomaticKeepAliveClientMixin {
   late VideoPlayerController videoPlayerController;
   ChewieController? chewieController;
   bool isLiked = false;
+  bool isPaused = false;
 
   @override
   void initState() {
     super.initState();
     videoPlayerController =
         VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
+
     // Initialize the video player controller asynchronously
     videoPlayerController.initialize().then((_) {
-      // Once the video is initialized, set up the Chewie controller
+      // If there's a saved position, seek to it
+      if (widget.savedPosition != null) {
+        videoPlayerController
+            .seekTo(Duration(seconds: widget.savedPosition!.toInt()));
+      }
+
+      // Set up the Chewie controller
       chewieController = ChewieController(
         videoPlayerController: videoPlayerController,
-        autoPlay: true,
+        autoPlay: false, // Disable autoPlay, we'll handle it manually
         looping: true,
         allowFullScreen: false,
+        showControlsOnInitialize: false,
         materialProgressColors: ChewieProgressColors(
           playedColor: Colors.white,
           handleColor: AppColors.primary,
@@ -56,10 +72,16 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
 
   @override
   void dispose() {
+    // Save the current position when the video is disposed
+    widget.onPositionChanged(widget.videoUrl,
+        videoPlayerController.value.position.inSeconds.toDouble());
     videoPlayerController.dispose();
     chewieController?.dispose();
     super.dispose();
   }
+
+  @override
+  bool get wantKeepAlive => true; // Ensure that the widget is kept alive
 
   void toggleLike() {
     setState(() {
@@ -71,8 +93,31 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
     print("Sharing the video...");
   }
 
+  // Manually play/pause when the video becomes visible
+  void onVisibilityChanged(VisibilityInfo visibilityInfo) {
+    final isCurrentlyPlaying =
+        chewieController?.videoPlayerController.value.isPlaying ?? false;
+
+    if (visibilityInfo.visibleFraction >= 0.7 && !isCurrentlyPlaying) {
+      chewieController?.videoPlayerController.play();
+      if (mounted) {
+        setState(() {
+          isPaused = false;
+        });
+      }
+    } else if (visibilityInfo.visibleFraction < 0.7 && isCurrentlyPlaying) {
+      if (mounted) {
+        chewieController?.videoPlayerController.pause();
+        setState(() {
+          isPaused = false;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     // Show shimmer effect while the video is initializing
     if (!videoPlayerController.value.isInitialized) {
       return Center(
@@ -84,7 +129,6 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
             height: MediaQuery.of(context).size.height - 120,
             width: double.infinity,
             child: const Center(
-              // Empty center since we don't want any loading spinner
               child: SizedBox.shrink(),
             ),
           ),
@@ -92,106 +136,110 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
       );
     }
 
-    return Container(
-      color: Colors.black,
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.center,
+    return VisibilityDetector(
+      key: Key(widget.videoUrl),
+      onVisibilityChanged: onVisibilityChanged,
+      child: Container(
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height - 120,
+                  child: AspectRatio(
+                    aspectRatio: videoPlayerController.value.aspectRatio,
+                    child: chewieController != null
+                        ? Chewie(controller: chewieController!)
+                        : const SizedBox.shrink(),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 10,
+              bottom: 170,
               child: SizedBox(
-                height: MediaQuery.of(context).size.height - 120,
-                child: AspectRatio(
-                  aspectRatio: videoPlayerController.value.aspectRatio,
-                  child: chewieController != null
-                      ? Chewie(controller: chewieController!)
-                      : const SizedBox.shrink(),
+                height: 40,
+                width: 200,
+                child: ListTile(
+                  contentPadding: const EdgeInsets.only(top: 10),
+                  dense: true,
+                  horizontalTitleGap: 5,
+                  visualDensity: VisualDensity.comfortable,
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage:
+                        CachedNetworkImageProvider(widget.profileUrl),
+                  ),
+                  title: widget.title.isEmpty
+                      ? const Text(
+                          'Nil',
+                          style: TextStyle(fontSize: AppSize.s14),
+                        )
+                      : widget.title.length < 20
+                          ? Text(
+                              widget.title,
+                              style: const TextStyle(fontSize: AppSize.s14),
+                            )
+                          : Marquee(
+                              text: widget.title,
+                              velocity: 10,
+                              blankSpace: 20,
+                              numberOfRounds: 1,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              style: const TextStyle(fontSize: AppSize.s14),
+                            ),
+                  subtitle: widget.description.isEmpty
+                      ? const Text(
+                          'Nil',
+                          style: TextStyle(fontSize: AppSize.s14),
+                        )
+                      : widget.description.length < 20
+                          ? Text(
+                              widget.description,
+                              style: const TextStyle(fontSize: AppSize.s12),
+                            )
+                          : Marquee(
+                              text: widget.description,
+                              velocity: 20,
+                              blankSpace: 20,
+                              numberOfRounds: 2,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              style: const TextStyle(fontSize: AppSize.s12),
+                            ),
+                  subtitleTextStyle: const TextStyle(fontSize: AppSize.s12),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: 10,
-            bottom: 170,
-            child: SizedBox(
-              height: 40,
-              width: 200,
-              child: ListTile(
-                contentPadding: const EdgeInsets.only(top: 10),
-                dense: true,
-                horizontalTitleGap: 5,
-                visualDensity: VisualDensity.comfortable,
-                leading: CircleAvatar(
-                  radius: 30,
-                  backgroundImage:
-                      CachedNetworkImageProvider(widget.profileUrl),
-                ),
-                title: widget.title.isEmpty
-                    ? const Text(
-                        'Nil',
-                        style: TextStyle(fontSize: AppSize.s14),
-                      )
-                    : widget.title.length < 20
-                        ? Text(
-                            widget.title,
-                            style: const TextStyle(fontSize: AppSize.s14),
-                          )
-                        : Marquee(
-                            text: widget.title,
-                            velocity: 10,
-                            blankSpace: 20,
-                            numberOfRounds: 1,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            style: const TextStyle(fontSize: AppSize.s14),
-                          ),
-                subtitle: widget.description.isEmpty
-                    ? const Text(
-                        'Nil',
-                        style: TextStyle(fontSize: AppSize.s14),
-                      )
-                    : widget.description.length < 20
-                        ? Text(
-                            widget.description,
-                            style: const TextStyle(fontSize: AppSize.s12),
-                          )
-                        : Marquee(
-                            text: widget.description,
-                            velocity: 20,
-                            blankSpace: 20,
-                            numberOfRounds: 2,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            style: const TextStyle(fontSize: AppSize.s12),
-                          ),
-                subtitleTextStyle: const TextStyle(fontSize: AppSize.s12),
+            Positioned(
+              bottom: 140,
+              right: 20,
+              child: Column(
+                spacing: 10,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      isLiked ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                    onPressed: toggleLike,
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.share,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                    onPressed: shareVideo,
+                  ),
+                ],
               ),
             ),
-          ),
-          Positioned(
-            bottom: 140,
-            right: 20,
-            child: Column(
-              spacing: 10,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    isLiked ? Icons.favorite : Icons.favorite_border,
-                    color: Colors.white,
-                    size: 35,
-                  ),
-                  onPressed: toggleLike,
-                ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.share,
-                    color: Colors.white,
-                    size: 35,
-                  ),
-                  onPressed: shareVideo,
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
